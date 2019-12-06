21813fc56b5a93eae2f3f98acfb245ce448653f1
MackieZhou <mzhou@smith.edu>
2019-12-06 13:13:58 -0500

    gameText class created

---
+++ gameText.py
@@ -0,0 +1,34 @@
+import pygame
+pygame.init()
+
+
+class gameText():
+    """create a text object with a rectangle bg under it"""
+
+    def __init__(self, text, textSize, textcolor, textpos, bgSize, bgcolor):
+        """text is a string; txtSize is an integer;
+        textpos is a tuple (x, y) which represent the location
+        where you will draw you text;
+        text color and bgcolor are tuple (red, green, blue);
+        bgSize is a tuple (x, y, width, length),
+        where (x, y) represents the top-left corner of the bg rectangle;
+        win is the window on which you want to draw this text object"""
+
+        self.txt = text
+        self.textcolor = textcolor
+        self.textSize = textSize
+        self.textpos = textpos
+
+        self.bgcolor = bgcolor
+
+        font = pygame.font.SysFont("arial", self.textSize, True, False)
+        self.text = font.render(self.txt, True, self.textcolor)
+
+        self.bg = pygame.Rect(bgSize[0], bgSize[1], bgSize[2], bgSize[3])
+
+    def draw(self, win):
+        """draw the text and its bg on a window"""
+        # first draw a bg rectangle
+        pygame.draw.rect(win, self.bgcolor, self.bg)
+        # then draw the text
+        win.blit(self.text, self.textpos)
